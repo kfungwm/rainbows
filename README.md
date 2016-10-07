bf026d4677adc4856a29ca6189566bf063c22045
Kevin Fung <kevinfung87@gmail.com>
2016-10-07 15:42:43 +0200

    Update the rainbows

---
+++ README.md
@@ -0,0 +1,4 @@
+If you in your browser write the code:
+node index.js 'name'
+
+it wil show the 'name' in rainbow colors. 
--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 var colors = require('colors');
 
-var myString = 'Hello World';
+function makeRainbows(string) {
 
-function makeRainbows() {
-
-  console.log(myString);
-  console.log(myString.rainbow);
-  console.log(myString.red);
-  console.log(myString.yellow);
-  console.log(myString.blue);
-  console.log(myString.green);
-  console.log(myString.cyan);
-  console.log(myString.magenta);
+  console.log(colors);
+  console.log(colors.rainbow(string));
+  console.log(colors.red(string));
+  console.log(colors.yellow(string));
+  console.log(colors.blue(string));
+  console.log(colors.green(string));
+  console.log(colors.cyan(string));
+  console.log(colors.magenta(string));
 }
 
-setInterval(makeRainbows, 9000);
+for (var i = 2; i < process.argv.length; i++) {
+setInterval(makeRainbows, 1000, process.argv[i]);
+}

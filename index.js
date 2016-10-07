0a6e8a23c9b2e4491e5d7d7a18a81608476ba8ba
Kevin Fung <kevinfung87@gmail.com>
2016-10-07 13:32:04 +0200

    update new rainbows

---
+++ index.js
@@ -0,0 +1,7 @@
+var colors = require('colors');
+
+console.log('OMG rainbows' .rainbow);
+console.log('OMG rainbows' .red);
+console.log('OMG rainbows' .yellow);
+console.log('OMG rainbows' .blue);
+console.log('OMG rainbows' .green);
